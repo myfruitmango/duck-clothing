4965bde5cf3840877cae01931c3dbb7f6314be8d
myfruitmango <ctrl.fruitman90@gmail.com>
2024-01-16 15:04:46 +0700

    React Router Outlet

--- src/App.js
+++ src/App.js
@@ -2,10 +2,16 @@ import { Routes, Route } from "react-router";
 
 import Home from "./routes/home/home.component";
 
+const Shop = () => {
+  return <h1>I am The Shop Page</h1>;
+};
 const App = () => {
   return (
     <Routes>
-      <Route path="/" element={<Home /> } />
+      <Route path="/home" element={<Home />}>
+        <Route path="shop" element={<Shop />} />
+        {/* /home/shop */}
+      </Route>
     </Routes>
   );
 };
