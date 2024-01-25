f39f7d4011ab2d5e1138620e3c65443abe6edd1b
fadjar aidil <m.fadjar@uni.net.id>
2024-01-25 17:02:32 +0700

    Observer Pattern

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -84,4 +84,13 @@ export const signInAuthUserWithEmailAndPassword = async (email, password) => {
 export const signOutUser = async () => await signOut(auth);
 
 export const onAuthStateChangedListener = (callback) =>
-  onAuthStateChanged(auth, callback);
+  onAuthStateChanged(auth, callback, errorCallback, completedCallback);
+
+
+  /**
+   * {
+   * @param next: callback
+   * @param error: errorCallback
+   * @param complete: completedCallback
+   * }
+   */
