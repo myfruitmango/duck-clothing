704dc030d7889a1450a94602b9d00890a26fcdf7
Fadjar Aidil <m.fadjar@uni.net.id>
2024-01-31 16:16:37 +0700

    Using Our CategoriesMap

--- src/contexts/categories.context.jsx
+++ src/contexts/categories.context.jsx
@@ -4,24 +4,27 @@ import { getCategoriesAndDocuments } from "../utils/firebase/firebase.utils.js";
 
 // import SHOP_DATA from "../shop-data.js";
 
-export const ProductContext = createContext({
-  product: [],
+export const CategoriesContext = createContext({
+  categoriesMap: {},
 });
 
-export const ProductsProvider = ({ children }) => {
-  const [products, setProducts] = useState([]);
+export const CategoriesProvider = ({ children }) => {
+  const [categoriesMap, setCategoriesMap] = useState({});
 
   useEffect(() => {
     const getCategoriesMap = async () => {
       const categoryMap = await getCategoriesAndDocuments();
       console.log(categoryMap);
+      setCategoriesMap(categoryMap);
     };
     getCategoriesMap();
     // addCollectionAndDocuments("categories", SHOP_DATA);
   }, []);
 
-  const value = { products };
+  const value = { categoriesMap };
   return (
-    <ProductContext.Provider value={value}>{children}</ProductContext.Provider>
+    <CategoriesContext.Provider value={value}>
+      {children}
+    </CategoriesContext.Provider>
   );
 };
