e338ea7c2e189453a1ed8c3c4a97c29f7f0efcb8
Fadjar Aidil <m.fadjar@uni.net.id>
2024-01-31 16:56:05 +0700

    Nested Routes in Shop

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,24 +1,14 @@
-import { useContext } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import { CategoriesContext } from "../../contexts/categories.context";
-
-import ProductCard from "../../components/product-card/product-card.component";
-import CategoryPreview from "../../components/category-perview/category-preview.component";
+import CategoriesPreview from "../categories-preview/categories-preview.component";
 
 import "./shop.styles.scss";
 
 const Shop = () => {
-  const { categoriesMap } = useContext(CategoriesContext);
-
   return (
-    <div className="shop-container">
-      {Object.keys(categoriesMap).map((title) => {
-        const products = categoriesMap[title];
-        return (
-          <CategoryPreview key={title} title={title} products={products} />
-        );
-      })}
-    </div>
+    <Routes>
+      <Route index element={<CategoriesPreview />} />
+    </Routes>
   );
 };
 export default Shop;
