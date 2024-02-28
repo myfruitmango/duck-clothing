e6a6d69b02c711e6d01e3d063f1042fa730cc3f3
m.fadjar@uni.net.id <ctrl.fruitman90@gmail.com>
2024-02-28 21:03:56 +0700

    Business Logic in Our Selectors

--- src/store/categories/categories.action.js
+++ src/store/categories/categories.action.js
@@ -1,5 +1,5 @@
 import { CATEGORIESS_ACTION_TYPES } from "./category.types";
 import { createAction } from "../../utils/reducer/reducer.utils";
 
-export const setCategoriesMap = (categoriesMap) =>
-  createAction(CATEGORIESS_ACTION_TYPES.SET_CATEGORIES_MAP, categoriesMap);
+export const setCategories = (categoriesArray) =>
+  createAction(CATEGORIESS_ACTION_TYPES.SET_CATEGORIES, categoriesArray);
