29c80b53a18bffb66a033a9100f10a971b7cd9ff
fadjaraidil <m.fadjar@uni.net.id>
2024-03-16 14:29:37 +0700

    Redux-Persist

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 // import logger from "redux-logger";
 
 import { rootReducer } from "./reducer";
@@ -16,9 +18,22 @@ const loggerMiddleware = (store) => (next) => (action) => {
 
   console.log("next state:", store.getState());
 };
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 // root-reducer
 const middleWares = [loggerMiddleware]; // bisa menggunakan
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
