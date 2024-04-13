3dfd152452a391bbe1049af1d48852ddd79e3049
myfruitmango <ctrl.fruitman90@gmail.com>
2024-04-13 14:08:02 +0700

    Redux-Saga: fetchCategoriesAsync Thunk to Saga

--- src/store/saga.js
+++ src/store/saga.js
@@ -1,5 +1,7 @@
 import { all, call } from "redux-saga/effects";
 
-export function* rootSaga () {
+import { CategoriesSaga } from "./categories/categories.saga";
 
+export function* rootSaga() {
+  yield all([call(CategoriesSaga)]);
 }
