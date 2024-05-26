6a0247f32ae6b2262ff7725db5e869d1eb2fc421
myfruitmango <ctrl.fruitman90@gmail.com>
2024-05-26 08:30:16 +0700

    fix: name cart icon

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,7 +6,7 @@ import { selectIsCartOpen } from "../../store/cart/cart.selector";
 import { selectCurrentUser } from "../../store/user/user.selector";
 import { signOutStart } from "../../store/user/user.action"
 
-import CartIcon from "../../components/cart-icon/cart-icon.componet";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
 import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
